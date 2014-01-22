ac3e157fe25fa1656c5c000a865dec1ea9694ab9
Neel Smith <dnsmith.neel@gmail.com>
2014-01-22 15:49:34 -0500

    Add remaining service ttl methods.

---
+++ src/main/groovy/org/homermultitext/HmtCollectionTurtleizer.groovy
@@ -0,0 +1,95 @@
+package org.homermultitext
+
+import edu.holycross.shot.prestochango.*
+
+
+/**
+*
+*/
+class HmtCollectionTurtleizer {
+
+    boolean debug = true
+
+    File collectionInventory
+
+    File dataDirectory 
+
+    String inventorySource
+
+
+    /** Writable file for resulting turtle-formatted triplets. */
+    File turtleOutput
+
+    boolean includePrefix
+
+
+    HmtCollectionTurtleizer(File inv, String invSch, File srcDir, File outDir) {
+        this.collectionInventory =  inv
+        this.inventorySource = invSch
+        this.dataDirectory = srcDir
+        this.turtleOutput = new File(outDir, "collections.ttl")
+        this.includePrefix = false
+    }
+
+    HmtCollectionTurtleizer(File inv, String invSch, File srcDir, File outDir, boolean prefix) {
+        this.collectionInventory =  inv
+        this.inventorySource = invSch
+        this.dataDirectory = srcDir
+        this.turtleOutput = new File(outDir, "collections.ttl")
+        this.includePrefix = prefix
+    }
+
+
+    /** 
+    * main() method expects four arguments: 
+    */
+    public static void main(String[] args) 
+    throws Exception {
+
+        switch (args.size()) {
+            case 0:
+                throw new Exception("main method requires three parameters.")
+            System.exit(-1)
+            break
+
+            case 4:
+                try {
+                
+                File inv = new File(args[0])
+		String invSchema = args[1]
+                File dataDir = new File(args[2])
+                File outDir = new File(args[3])
+                if (! outDir.exists()) {
+                    outDir.mkdir()
+                }
+
+		println "USE URL " + invSchema
+		try {
+		  URL u = new URL(invSchema)
+		} catch (Exception e) {
+		  println "Couldn't make url from " + invSchema
+		}
+
+                boolean prefix = true
+                HmtCollectionTurtleizer pcttl = new HmtCollectionTurtleizer(inv, invSchema, dataDir, outDir, prefix)
+                pcttl.generateTurtle()
+
+            } catch (Exception e) {
+                throw e
+            }
+            break
+
+
+            default:
+            break
+        }
+    }
+
+
+    void generateTurtle() {
+        CollectionArchive cc = new CollectionArchive(this.collectionInventory, this.inventorySource, this.dataDirectory)
+
+        cc.ttl(turtleOutput, includePrefix)
+    }
+
+}
---
+++ src/main/groovy/org/homermultitext/HmtIndexTurtleizer.groovy
@@ -0,0 +1,85 @@
+package org.homermultitext
+
+import edu.holycross.shot.abracadabra.*
+
+
+/**
+*
+*/
+class HmtIndexTurtleizer {
+
+    boolean debug = false
+
+    File inventory
+
+    File data
+
+
+    /** Writable file for resulting turtle-formatted triplets. */
+    File turtleOutput
+
+    boolean includePrefix
+
+    HmtIndexTurtleizer(File inv, File srcDir, File outFile) {
+        this.inventory =  inv
+        this.data = srcDir
+        this.turtleOutput = outFile
+        this.includePrefix = false
+    }
+
+    HmtIndexTurtleizer(File inv, File srcDir, File outFile, boolean prefix) {
+        this.inventory =  inv
+        this.data = srcDir
+        this.turtleOutput = outFile
+        this.includePrefix = prefix
+    }
+
+
+    /** 
+    * main() method expects four arguments:
+    */
+    public static void main(String[] args) 
+    throws Exception {
+
+        switch (args.size()) {
+            case 0:
+                throw new Exception("main method requires five parameters.")
+            System.exit(-1)
+            break
+
+            case 4:
+                try {
+                File inv = new File(args[0])
+                File dataDir = new File(args[1])
+                File outDir = new File(args[2])
+                if (! outDir.exists()) {
+                    outDir.mkdir()
+                }
+                File outFile = new File(outDir, args[3])
+
+                boolean prefix = true
+                if ( (!inv.exists())  || (!dataDir.exists())) {
+                    System.err.println "HmtIndexTurtelizer:  ${inv} and ${dataDir} must exist."
+                } else {
+                    HmtIndexTurtleizer ttl = new HmtIndexTurtleizer(inv, dataDir, outFile, prefix)
+                    ttl.generateTurtle()
+                }
+
+            } catch (Exception e) {
+                throw e
+            }
+            break
+
+
+            default:
+            break
+        }
+    }
+
+
+    void generateTurtle() {
+        CiteIndex idx = new CiteIndex(inventory, data)
+        idx.ttl(turtleOutput, includePrefix)
+    }
+
+}
