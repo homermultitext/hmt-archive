40cf1a1a52a383e09e0f025f6bd8514213179cbb
Neel Smith <dnsmith.neel@gmail.com>
2014-02-04 23:38:41 +0100

    Add book 8 material.

--- src/main/groovy/org/homermultitext/ScholiaIndexer.groovy
+++ src/main/groovy/org/homermultitext/ScholiaIndexer.groovy
@@ -33,7 +33,7 @@ class ScholiaIndexer {
     CtsUrn scholiaUrn = new CtsUrn("urn:cts:greekLit:tlg5026")
 
     /** Verbosity level 0-3 of debugging output */
-    Integer debug = 1
+    Integer debug = 3
 
 
 
@@ -47,51 +47,54 @@ class ScholiaIndexer {
 
 
     void writeIndex() {
-        File scholiaIndex = new File(outputDirectory, indexFileName)
-        xmlSourceDirectory.eachFileMatch(~/.*\.xml/) { srcFile ->
-            System.err.println "Indexing scholia in " + srcFile
-            try {
-                def fRoot = new XmlParser().parse(srcFile)
-                def groupNode = fRoot[tei.text][tei.group][0]
-                def bk = groupNode.'@n'
+      File scholiaIndex = new File(outputDirectory, indexFileName)
+      xmlSourceDirectory.eachFileMatch(~/.*\.xml/) { srcFile ->
+	System.err.println "Indexing scholia in " + srcFile
+	try {
+	  def fRoot = new XmlParser().parse(srcFile)
+	  def groupNode = fRoot[tei.text][tei.group][0]
+	  def bk = groupNode.'@n'
 
-                groupNode[tei.text].each { txt ->
-
-                    String workId = txt.'@n'
-                    String bookLevel = "${scholiaUrn}.${workId}.${version}:${bk}"
-                    if (debug) {System.err.println "Indexing book ${bookLevel}"}
-
-                    txt[tei.body][tei.div].each { schol ->
-                        String scholionId = schol.'@n'
-
-                        schol[tei.div].each { d ->
-                            switch (d.'@type') {
-                                case "ref":
-                                    // values must be valid CTS URNs
-                                    CtsUrn scholUrn
-                                CtsUrn psgUrn
-                                try {
-                                    scholUrn = new CtsUrn("${bookLevel}.${scholionId}")
-                                } catch (Exception e) {
-                                    System.err.println "Bad URN value for scholion ${bookLevel}.${scholionId}"
-                                }
-                                try {
-                                    // NS CHANGE THIS:  INSIST ON FULL URN IN SOURCE DOC
-                                    psgUrn = new CtsUrn("${iliadUrn}" + d.p[0].text())
-                                } catch (Exception e) {
-                                    System.err.println "Bad URN value for Iliad passage ${psgUrn}:" + d.p[0].text() + " (reference in scholion ${scholUrn})"
-                                }
-
-                                
-                                scholiaIndex.append("${scholUrn}\t${psgUrn}\n")
-                                break
+	  groupNode[tei.text].each { txt ->
+	    String workId = txt.'@n'
+	    String bookLevel = "${scholiaUrn}.${workId}.${version}:${bk}"
+	    if (debug > 0) {System.err.println "Indexing book ${bookLevel}"}
+	    
+	    txt[tei.body][tei.div].each { schol ->
+	      String scholionId = schol.'@n'
+	      System.err.println "\tScholion id is " + scholionId
+	      schol[tei.div].each { d ->
+		switch (d.'@type') {
+		case "ref":
+		// values must be valid CTS URNs
+		CtsUrn scholUrn
+		CtsUrn psgUrn
+		try {
+		  scholUrn = new CtsUrn("${bookLevel}.${scholionId}")
+		} catch (Exception e) {
+		  System.err.println "Bad URN value for scholion ${bookLevel}.${scholionId}"
+		}
 
 
-                                default :
-                                    break
-                            }
-                        }
 
+		try {
+		  // NS CHANGE THIS:  INSIST ON FULL URN IN SOURCE DOC
+		  //psgUrn = new CtsUrn("${iliadUrn}" + d.p[0].text())
+		  psgUrn = new CtsUrn(d.p[0].text())
+		} catch (Exception e) {
+		System.err.println "Bad URN value for Iliad passage ${psgUrn}:" + d.p[0].text() + " (reference in scholion ${scholUrn})"
+		}
+
+                        
+			scholiaIndex.append("${scholUrn}\t${psgUrn}\n")
+			break
+
+
+			default :
+			break
+			}
+		      }
+		      
                     }
 
                 }
