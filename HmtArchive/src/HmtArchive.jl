4d6e7a87c9cfdefc54d8fe5b86e3b44de7ab8713
Neel Smith <dnsmith.neel@gmail.com>
2021-02-03 08:43:11 -0500

    Start grouping scholia

--- HmtArchive/src/HmtArchive.jl
+++ HmtArchive/src/HmtArchive.jl
@@ -7,7 +7,10 @@ using EditionBuilders
 using EzXML
 
 
+# Diplomatic edition of VA Iliad
 export iliaddipl
+
+# Useful to make declaration of TEI namespace global
 export teins
 
 include("textbuilding.jl")
--- HmtArchive/src/textbuilding.jl
+++ HmtArchive/src/textbuilding.jl
@@ -28,20 +28,32 @@ function iliadxmlcorpus()
 end
 
 function groupscholia()
+    bookxp = "/ns:TEI/ns:text/ns:group"
+    docxp = "/ns:TEI/ns:text/ns:group/ns:text"
+    nodexp = "/ns:TEI/ns:text/ns:group/ns:text/ns:body/ns:div"
     scholiagroup = CtsUrn("urn:cts:greekLit:tlg5026:")
     scholiasrc = dirname(pwd()) * "/archive/scholia/"
     scholiafiles = filter(f -> endswith(f, "xml"), readdir(scholiasrc))
     fullpath = map(f -> scholiasrc * f, scholiafiles)
     docs = [] 
     for f in fullpath
-        contents = open(f) do file
-            read(file, String)
-        end
         try 
-            push!( docs, readxml(contents))
+            booklevel = findall(bookxp, doc,["ns"=> teins]) 
+            book = booklevel[1]["n"]
+            scholiadocs = findall(docxp, doc,["ns"=> teins]) 
+            sigla = map(root -> root["n"], scholiadocs)
+            # nodexp
+            # Now find nodes for each doc
+            # Use method call directly in CitableTei lib to convert xml node to 
+            # Citable node
+            
         catch e
-            println("ERROR ON $(f) : $(e)")
+            throw(DomainError("ERROR ON $(f) : $(e)"))
         end
     end
-    reverse!(docs)
+
+
+    
+   
+    return reverse!(docs)
 end
