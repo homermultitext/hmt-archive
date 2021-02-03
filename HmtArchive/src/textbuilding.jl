f8ce8d246357a1274f240938f24a9cc1e74ed702
Neel Smith <nsmith@Technites.local>
2021-02-03 08:02:33 -0500

    Start on grouping scholia

--- HmtArchive/src/textbuilding.jl
+++ HmtArchive/src/textbuilding.jl
@@ -26,3 +26,22 @@ function iliadxmlcorpus()
     end
     composite_array(reverse!(corpora))
 end
+
+function groupscholia()
+    scholiagroup = CtsUrn("urn:cts:greekLit:tlg5026:")
+    scholiasrc = dirname(pwd()) * "/archive/scholia/"
+    scholiafiles = filter(f -> endswith(f, "xml"), readdir(scholiasrc))
+    fullpath = map(f -> scholiasrc * f, scholiafiles)
+    docs = [] 
+    for f in fullpath
+        contents = open(f) do file
+            read(file, String)
+        end
+        try 
+            push!( docs, readxml(contents))
+        catch e
+            println("ERROR ON $(f) : $(e)")
+        end
+    end
+    reverse!(docs)
+end
