06268bc61fe5005f624def4ed75ca96c06ebb62e
Neel Smith <dnsmith.neel@gmail.com>
2025-01-11 15:29:14 -0500

    Generate 2 more

--- buildiiif.jl
+++ buildiiif.jl
@@ -40,3 +40,56 @@ vaoutfile = joinpath(outdir, "venetusA.json")
 open(vaoutfile,"w") do io
     write(io, vajson)
 end
+
+
+
+msT = mss[1]
+burneymanifest =  "https://raw.githubusercontent.com/homermultitext/hmt-archive/refs/heads/master/iiif/burney86.json"
+burneyconfig = iiifconfig(burneymanifest)
+burneyjson = iiifmanifest(msT, burneyconfig, imgsvc)
+
+isdir(outdir)
+burneyoutfile = joinpath(outdir, "burney86.json")
+open(burneyoutfile,"w") do io
+    write(io, burneyjson)
+end
+
+
+
+#= NFG
+e3 = mss[2]
+e3manifest =  "https://raw.githubusercontent.com/homermultitext/hmt-archive/refs/heads/master/iiif/escorial_upsilon_1_1.json"
+e3config = iiifconfig(e3manifest)
+
+e3json = iiifmanifest(e3, e3config, imgsvc)
+
+e3outfile = joinpath(outdir, "upsilon_1_1.json")
+open(e3outfile,"w") do io
+    write(io, e3json)
+end
+
+
+e4 = mss[3]
+e4manifest =  "https://raw.githubusercontent.com/homermultitext/hmt-archive/refs/heads/master/iiif/escorial_omega_1_12.json"
+e4config = iiifconfig(e4manifest)
+
+e4json = iiifmanifest(e4, e4config, imgsvc)
+
+e4outfile = joinpath(outdir, "omega_1_12.json")
+open(e4outfile,"w") do io
+    write(io, e4json)
+end
+=#
+mss
+
+
+m841 = mss[5]
+m841manifest =  "https://raw.githubusercontent.com/homermultitext/hmt-archive/refs/heads/master/iiif/marciana841.json"
+m841config = iiifconfig(m841manifest)
+
+m841json = iiifmanifest(m841, m841config, imgsvc)
+
+m841outfile = joinpath(outdir, "marciana_841.json")
+open(m841outfile,"w") do io
+    write(io, m841json)
+end
---
+++ iiif/README.md
@@ -0,0 +1,9 @@
+# IIIF manifests
+
+Date generated: 2025, January 11
+
+Manuscripts covered:
+
+- [Venetus A](./venetusA.json)
+- [Venice, Biblioteca Marciana 841](./marciana_841.json)
+- [British Museum, Burney 86](./burney86.json)
